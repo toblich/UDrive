53f6c52074b44d684a6c8ae0518a6a9c31ca2dc1
Francisco <plandino@hotmail.com>
2015-09-09 12:46:13 -0300

    Subo el cmake que genera el makefile que funciona para eclipse. Ademas subo la arpeta DB donde se va a guardar la base de datos y el constructor de la base de datos.

--- include/basededatos.h
+++ include/basededatos.h
@@ -1,14 +1,37 @@
 #ifndef BASEDEDATOS_H_
 #define BASEDEDATOS_H_
 
-using namespace rocksdb;
+#include <string>
+#include <iostream>
+
+#include <rocksdb/db.h>
+#include <rocksdb/slice.h>
+#include <rocksdb/options.h>
+
+
+
+std::string kDBPath = "DB/basededatos";
 
 class BaseDeDatos {
 
 private:
+	  rocksdb::DB* db;
+	  rocksdb::Options options;
 
 public:
 	BaseDeDatos();
+//	{
+//		  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+//		  options.IncreaseParallelism();
+//		  options.OptimizeLevelStyleCompaction();
+//		  // create the DB if it's not already present
+//		  options.create_if_missing = true;
+//
+//		  // open DB
+//		  Status s = DB::Open(options, kDBPath, &db);
+//		  std::cout << s.ToString() << std::endl;
+//		  assert(s.ok());
+//	}
 	~BaseDeDatos();
 
 };
--- src/basededatos.cpp
+++ src/basededatos.cpp
@@ -1,9 +1,20 @@
 #include "basededatos.h"
 
-BaseDeDatos::BaseDeDatos(){
+using namespace rocksdb;
 
+BaseDeDatos::BaseDeDatos(){
+	  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+	  options.IncreaseParallelism();
+	  options.OptimizeLevelStyleCompaction();
+	  // create the DB if it's not already present
+	  options.create_if_missing = true;
+
+	  // open DB
+	  Status s = DB::Open(options, kDBPath, &db);
+	  std::cout << s.ToString() << std::endl;
+	  assert(s.ok());
 }
 
 BaseDeDatos::~BaseDeDatos(){
-
+	  delete db;
 }
