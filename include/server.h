145b5f71f50811c9f3ee4da07f9a988d332187e8
Tobi <toblich@gmail.com>
2015-09-21 17:29:45 -0300

    reemplaza uso de BaseDeDatos por uso de la interfaz DB

--- include/server.h
+++ include/server.h
@@ -4,28 +4,29 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "logger.h"
 #include "mongoose.h"
-#include "basededatos.h"
+#include "bd.h"
 #include "manejadorDeUsuarios.h"
 
 using namespace std;
 
 class Server {
 public:
-	Server(std::string portNumber);
+	Server(string portNumber, BD* perfiles, BD* sesiones, BD* passwords);
 	virtual ~Server();
 
 	static int eventHandler(mg_connection *connection, mg_event event);
 	static string mensajeSegunURI(string uri);
 	void pollServer(int milliseconds);
-	const char* getListeningPort() { return mg_get_option(server, "listening_port");}
+	const char* getListeningPort() { return mg_get_option(server, "listening_port"); }
 
 private:
 	mg_server* server;
 	ManejadorDeUsuarios* manejador;
-	BaseDeDatos* perfiles;
-	BaseDeDatos* sesiones;
-	BaseDeDatos* passwords;
+	BD* perfiles;
+	BD* sesiones;
+	BD* passwords;
 
 	static void closeHandler(mg_connection* connection);
 	static void requestHandler(mg_connection* connection);
