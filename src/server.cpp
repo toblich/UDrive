bb3c978f6619d41a4029d89d207ec317d3c2f7a8
Santiago <fernandezsantid@gmail.com>
2015-10-09 13:16:06 -0300

    Agregue mas test del servidor: Borrar carpetas, uri inexistente, recurso inexistente, metodos no soportados de recursos existentes #31

--- src/server.cpp
+++ src/server.cpp
@@ -100,10 +100,12 @@ mg_result Server::requestHandler(mg_connection* connection) {
 			return evento->handler(connection);
 
 		}catch (const out_of_range& oor){
+			mg_send_status(connection, NOTFOUND);
 			mg_printf_data(connection, "Error, recurso no encontrado\n");
 			return MG_TRUE;
 		}
 	}
-	mg_printf_data(connection, "Error, URI incorrecta\n");
+	mg_send_status(connection, NOTFOUND);
+	mg_printf_data(connection, "Error, recurso no encontrado\n");
 	return MG_TRUE;
 }
