9462b29943e015e4a84705793b3df4f610ec03d8
Tobi <toblich@gmail.com>
2015-10-29 18:31:47 -0300

    Vuelve a poner el sleep de travis en 0.1s

--- test/server_test.py
+++ test/server_test.py
@@ -79,7 +79,7 @@ class ServerTest(unittest.TestCase):
 		if exists("FileSystem"):
 			rmtree("FileSystem")
 		self.serverProcess = Popen(["./udrive", "&"])
-		sleep(0.01)
+		sleep(0.1)
 
 
 	def tearDown(self):
